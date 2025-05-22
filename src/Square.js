3689a9574f8868b71b06ec90b4b71d3d43455f0c
Vinicius G. O. Pontes <viniciuspontesfx059@gmail.com>
2025-05-22 13:37:23 -0400

    Add files via upload

---
+++ src/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Square({ value, onClick, disabled }) {
+  return (
+    <button className="square" onClick={onClick} disabled={disabled}>
+      {value}
+    </button>
+  );
+}
+
+export default Square;
